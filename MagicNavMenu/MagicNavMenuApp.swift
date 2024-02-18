9c50f225266e2cc03272b11fd36829e72addf4ef
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-02-18 22:04:01 +0600

    Initial Commit

---
+++ MagicNavMenu/MagicNavMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicNavMenuApp.swift
+//  MagicNavMenu
+//
+//  Created by Aizada on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MagicNavMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
