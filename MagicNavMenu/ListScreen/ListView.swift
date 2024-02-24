03b77ae08ed3eecf932037e13877168ea757a81d
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-02-24 15:37:32 +0600

    Added ListView file

--- MagicNavMenu/ListScreen/ListView.swift
+++ MagicNavMenu/ListScreen/ListView.swift
@@ -2,10 +2,34 @@
 import SwiftUI
 
 struct ListView: View {
+    @EnvironmentObject var goalsManager: GoalsManager
+    
     var body: some View {
         NavigationView {
             List {
-                Text("Goal")
+                ForEach(0..<goalsManager.goals.count, id: \.self) { index in
+                    HStack {
+                        Text(goalsManager.goals[index])
+                            .foregroundColor(goalsManager.isGoalCompleted[index] ? .green : .primary)
+                        
+                        Spacer()
+                        
+                        Image(systemName: goalsManager.isGoalCompleted[index] ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                goalsManager.toggleGoalCompletion(index: index)
+                            }
+                            .foregroundColor(goalsManager.isGoalCompleted[index] ? .green : .primary)
+                    }
+                    .contentShape(Rectangle())
+                    .swipeActions {
+                        Button(action: {
+                            goalsManager.removeGoal(at: index)
+                        }) {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
             }
             .navigationBarTitle("Goals")
             .navigationBarItems(trailing: EditButton())
