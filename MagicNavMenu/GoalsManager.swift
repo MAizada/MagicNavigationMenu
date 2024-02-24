21574e0023b97c2a052bf5d52d9e05f88ed49b49
MAizada <aizadamukhamedgaliyeva@gmail.com>
2024-02-24 15:27:55 +0600

    Updated file GoalsManager.swift

--- MagicNavMenu/GoalsManager.swift
+++ MagicNavMenu/GoalsManager.swift
@@ -1,8 +1,41 @@
-//
-//  GoalsManager.swift
-//  MagicNavMenu
-//
-//  Created by Aizada on 24.02.2024.
-//
 
-import Foundation
+import SwiftUI
+
+class GoalsManager: ObservableObject {
+    @Published var newGoalText: String = ""
+    @Published var goals: [String] = []
+    @Published var isGoalCompleted: [Bool] = []
+    
+    init() {
+        if let savedGoals = UserDefaults.standard.array(forKey: "savedGoals") as? [String] {
+            goals = savedGoals
+        }
+        
+        if let savedIsGoalCompleted = UserDefaults.standard.array(forKey: "savedIsGoalCompleted") as? [Bool] {
+            isGoalCompleted = savedIsGoalCompleted
+        }
+    }
+    
+    func addGoal() {
+        goals.append(newGoalText)
+        isGoalCompleted.append(false)
+        newGoalText = ""
+        saveToUserDefaults()
+    }
+    
+    func toggleGoalCompletion(index: Int) {
+        isGoalCompleted[index].toggle()
+        saveToUserDefaults()
+    }
+    
+    func removeGoal(at index: Int) {
+        goals.remove(at: index)
+        isGoalCompleted.remove(at: index)
+        saveToUserDefaults()
+    }
+    
+    private func saveToUserDefaults() {
+        UserDefaults.standard.set(goals, forKey: "savedGoals")
+        UserDefaults.standard.set(isGoalCompleted, forKey: "savedIsGoalCompleted")
+    }
+}
